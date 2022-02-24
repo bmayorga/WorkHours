6fc43aee09ab7c4968a20e901534b98fc22d335a
Byron Mayorga <bmayorga@gmail.com>
2022-02-24 17:29:45 -0500

    Schedule definition and processing.

---
+++ HourRates.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkHours
+{
+    class HourRate
+    {
+        public DateTime iniTime;
+        public DateTime endTime;
+        public ScheduleRules.DayType dayType;
+        public float Rate;
+    }
+    class HourRates
+    {
+        public static List<HourRate> GetSchedule()
+        {
+            List<HourRate> list = new List<HourRate>();
+
+            // Week days
+            list.Add(new HourRate
+            {
+                dayType = ScheduleRules.DayType.weekday,
+                iniTime = NewTime(0, 1),
+                endTime = NewTime(9, 0),
+                Rate = 25
+            });
+            list.Add(new HourRate
+            {
+                dayType = ScheduleRules.DayType.weekday,
+                iniTime = NewTime(9, 1),
+                endTime = NewTime(18, 0),
+                Rate = 15
+            });
+            list.Add(new HourRate
+            {
+                dayType = ScheduleRules.DayType.weekday,
+                iniTime = NewTime(18, 1),
+                endTime = NewTime(0, 0),
+                Rate = 20
+            });
+
+            //Weekend
+            list.Add(new HourRate
+            {
+                dayType = ScheduleRules.DayType.weekend,
+                iniTime = NewTime(0, 1),
+                endTime = NewTime(9, 0),
+                Rate = 30
+            });
+            list.Add(new HourRate
+            {
+                dayType = ScheduleRules.DayType.weekend,
+                iniTime = NewTime(9, 1),
+                endTime = NewTime(18, 0),
+                Rate = 20
+            });
+            list.Add(new HourRate
+            {
+                dayType = ScheduleRules.DayType.weekend,
+                iniTime = NewTime(18, 1),
+                endTime = NewTime(0, 0),
+                Rate = 25
+            });
+            return list;
+        }
+        public static DateTime NewTime(int hour, int min)
+        {
+            return new DateTime(1, 1, 1, hour, min, 0);
+        }
+    }
+}
---
+++ ScheduleRules.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkHours
+{
+    class ScheduleRules
+    {
+        public enum DayType { weekday, weekend };
+        public static DayType GetDayType(string day)
+        {
+            DayType dayType = new DayType();
+            switch (day)
+            {
+                case "MO":
+                case "TU":
+                case "WE":
+                case "TH":
+                case "FR":
+                    dayType = DayType.weekday;
+                    break;
+                case "SA":
+                case "SU":
+                    dayType = DayType.weekend;
+                    break;
+            }
+            return dayType;
+        }
+    }
+}
